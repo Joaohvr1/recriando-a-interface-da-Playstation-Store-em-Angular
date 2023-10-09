7a00b41d7a5676941da67fc27fb7cc9ff4b0b033
João Henrique <joaohenriquevidalribeiro@gmail.com>
2023-10-09 11:12:00 -0300

    created nav-bar

--- psn-store/src/app/app.module.ts
+++ psn-store/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    NavBarComponent,
+    CardLabelComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
